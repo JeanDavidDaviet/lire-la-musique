db1f3eecb5211bd8ca7f45cb997484d88e621657
Jean-David Daviet <jeandaviddaviet@gmail.com>
2018-08-13 16:30:45 +0200

    Add white background to signature

--- src/components/Signature/FlatFactory.js
+++ src/components/Signature/FlatFactory.js
@@ -5,12 +5,15 @@ import flats from '../../flats.js';
 const FlatFactory = ( { signature, chosenScale } ) => {
   const numberOfFlats = Object.keys(signature.flats).indexOf(chosenScale);
   let flatsObjects = [];
+  let widthOfBackground = 0;
   flats.map((value, index) => {
     if(index < numberOfFlats){
-      flatsObjects.push(<Flat x={-330 + ( 15 * index )} y={value} />)
+      flatsObjects.push(<Flat key={index} x={-320 + ( 15 * index )} y={value} />)
+      widthOfBackground++;
     }
   })
-  return Object.keys(signature.flats).indexOf(chosenScale) > -1 ? flatsObjects : 'null';
+  const background = <rect key="background" x="0" y="0" width={35 + widthOfBackground * 15} height="150" fill="white" style={{transform: "translate3d(0, -50px, 0px)"}} />;
+  return Object.keys(signature.flats).indexOf(chosenScale) > -1 ? [background, flatsObjects] : null;
 }
 
 export default FlatFactory;
--- src/components/Signature/SharpFactory.js
+++ src/components/Signature/SharpFactory.js
@@ -5,12 +5,15 @@ import sharps from '../../sharps.js';
 const SharpFactory = ( { signature, chosenScale } ) => {
   const numberOfSharps = Object.keys(signature.sharps).indexOf(chosenScale);
   let sharpsObjects = [];
+  let widthOfBackground = 0;
   sharps.map((value, index) => {
     if(index < numberOfSharps){
-      sharpsObjects.push(<Sharp x={-330 + ( 15 * index )} y={value} />)
+      sharpsObjects.push(<Sharp key={index} x={-330 + ( 15 * index )} y={value} />)
+      widthOfBackground++;
     }
   })
-  return Object.keys(signature.sharps).indexOf(chosenScale) > -1 ? sharpsObjects : 'null';
+  const background = <rect key="background" x="0" y="0" width={35 + widthOfBackground * 15} height="150" fill="white" style={{transform: "translate3d(0, -50px, 0px)"}} />;
+  return Object.keys(signature.sharps).indexOf(chosenScale) > -1 ? [background, sharpsObjects] : null;
 }
 
 export default SharpFactory;
