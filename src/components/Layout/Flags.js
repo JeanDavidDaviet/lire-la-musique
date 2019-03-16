1bba2d13c8e0af7ef942a5c1c540272913f496c3
Jean-David Daviet <jeandaviddaviet@gmail.com>
2019-03-16 14:26:28 +0100

    Add info on adding language

--- src/components/Layout/Flags.js
+++ src/components/Layout/Flags.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import i18n from "../../i18n";
+import Dialog from '@material-ui/core/Dialog';
+import Typography from '@material-ui/core/Typography';
+import { useTranslation } from 'react-i18next';
 
-const Flags = () => (
-    <div className="flags">
-        <img style={style} alt="Français" src="/assets/svg/fr.svg" onClick={() => i18n.changeLanguage('fr')} />
-        <img style={style} alt="English" src="/assets/svg/gb.svg" onClick={() => i18n.changeLanguage('en')} />
-    </div>
-);
+const Flags = () => {
+    const { t } = useTranslation();
+    const [opened, setOpen] = useState(false);
+    return (
+        <div className="flags">
+            <img style={style} alt="Français" src="/assets/svg/fr.svg" onClick={() => i18n.changeLanguage('fr')} />
+            <img style={style} alt="English" src="/assets/svg/gb.svg" onClick={() => i18n.changeLanguage('en')} />
+            <img style={style} alt="Other language" src="/assets/svg/language.svg" onClick={() => setOpen(true)} />
+            <Dialog open={opened} onClose={() => setOpen(!opened)} aria-labelledby="simple-dialog-title">
+                <Typography component="h2" variant="title" style={{ padding: 8 * 3 }}>{t('Contribute in your language!')}</Typography>
+                <div style={{padding: '0 24px 24px 24px'}}>
+                    <p>{t('You can contribute by adding your own translation in your language!')}</p>
+                    <p>{t('Two ways of doing this:')}</p>
+                    <ul>
+                        <li>{t('Send me an email at')} <a href="mailto:"></a></li>
+                        <li>{t('Contribute directly using')} <a href="https://github.com/JeanDavidDaviet/piano">Github</a></li>
+                    </ul>
+                </div>
+            </Dialog>
+        </div>
+    )
+};
 
 const style = {
     maxWidth: 24,
--- src/translations.js
+++ src/translations.js
@@ -8,14 +8,19 @@ export default {
       "Running": "Running",
       "Scale": "Scale",
       "Instrument": "Instrument",
+      "Time Signature": "Time Signature",
       "Tempo": "Tempo",
       "Piano": "Piano",
       "Computer": "Computer",
       "Treble": "Treble",
       "Bass": "Bass",
+      "Contribute in your language!": "Contribute in your language!",
+      "You can contribute by adding your own translation in your language!": "You can contribute by adding your own translation in your language!",
+      "Two ways of doing this:": "Two ways of doing this:",
+      "Send me an email at": "Send me an email at",
+      "Contribute directly using": "Contribute directly using",
     }
   },
-  "Time Signature": "Time Signature",
   fr: {
     translations: {
       "Read the music": "Lire la musique",
@@ -31,6 +36,11 @@ export default {
       "Computer": "Ordinateur",
       "Treble": "Sol",
       "Bass": "Fa",
+      "Contribute in your language!": "Contribuez dans votre langue !",
+      "You can contribute by adding your own translation in your language!": "Vous pouvez contribuer en ajoutant votre propre traduction dans votre langage !",
+      "Two ways of doing this:": "Deux façons de procéder :",
+      "Send me an email at": "Envoyez-moi un email à",
+      "Contribute directly using": "Contribuez directement avec",
     }
   }
 };
