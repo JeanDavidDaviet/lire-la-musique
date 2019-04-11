9801c828397632bdb37e6627f1c7b459a80ba13d
Jean-David Daviet <jeandaviddaviet@gmail.com>
2019-04-11 12:21:45 +0200

    Add Sentry to production

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import * as Sentry from '@sentry/browser';
+import config from './config';
+
+if(process.env.NODE_ENV === "production"){
+  Sentry.init({
+    environment: process.env.NODE_ENV,
+    release: config.version,
+    dsn: "https://66f76919249b4b1788b09d6c0f5f37e4@sentry.io/1409220"
+  });
+}
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
