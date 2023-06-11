3dae87e4b04c5573812eda159fd939e769a446b2
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2023-06-11 16:11:08 +0200

    generate stave given size of window

--- src/app.ts
+++ src/app.ts
@@ -11,16 +11,21 @@ const renderer = new Renderer(div, Renderer.Backends.SVG);
 // Configure the rendering context.
 renderer.resize(window.innerWidth, 200);
 const context = renderer.getContext();
+const windowSize = window.innerWidth;
+
+const barWidth  = 200;
+const barsToRender = Math.ceil((windowSize / 2) / barWidth);
+const barsToRenderWithPadding = barsToRender + 2;
 
 const svgChildren = (div.firstElementChild as SVGElement).children as HTMLCollectionOf<SVGGElement>;
 
-const generateStave = (oldStave?: Flow.Stave): Flow.Stave => {
+const generateStave = (oldStave: Flow.Stave | null): Flow.Stave => {
     const width = oldStave ? oldStave.getWidth() : 0;
-    // const x = oldStave ? oldStave.getX() : window.innerWidth / 2;
-    const x = oldStave ? oldStave.getX() : 0;
-    const staveMeasure = new Stave(width + x, 0, 200);
+    const x = oldStave ? oldStave.getX() : window.innerWidth / 2;
+    // const x = oldStave ? oldStave.getX() : 0;
+    const staveMeasure = new Stave(width + x, 0, barWidth);
     if (!oldStave) {
-        staveMeasure.addClef("treble").setContext(context).draw();
+        // staveMeasure.addClef("treble").setContext(context).draw();
     }
     const notesMeasure = [
         new StaveNote({keys: ["c/4"], duration: "q"}),
@@ -33,19 +38,23 @@ const generateStave = (oldStave?: Flow.Stave): Flow.Stave => {
 
     return staveMeasure
 }
-const staveMeasure1 = generateStave();
+const staves = [];
+for (let i = 0; i < barsToRenderWithPadding; i++){
+    const staveMeasure = generateStave(i > 0 ? staves[i - 1] : null);
+    staves.push(staveMeasure);
+}
 
 let scroll = 0;
-let distanceInPx = 100;
-let durationInMs = 1000;
+const distanceInPx = 200;
+const durationInMs = 4000;
+const stavePerMs = barWidth / distanceInPx * durationInMs
 function animateWidth(now: number) {
     requestAnimationFrame(animateWidth);
     scroll = now * distanceInPx / durationInMs;
 
     Array.from(svgChildren).forEach((c) => {
-        c.setAttribute('transform', `translate(${scroll} 0)`);
+        c.setAttribute('transform', `translate(${-scroll} 0)`);
     });
-
 }
 requestAnimationFrame(animateWidth);
 
