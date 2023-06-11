6d9644f90816f121bc049b20eb9c39d25b2ac392
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2023-06-11 15:39:24 +0200

    move sheet every frame by same pixels

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import {Stave, StaveNote, Formatter, Renderer} from "vexflow";
+import { Flow } from "vexflow";
+const Formatter = Flow.Formatter;
+const Renderer = Flow.Renderer;
+const Stave = Flow.Stave;
+const StaveNote = Flow.StaveNote;
 
 // Create an SVG renderer and attach it to the DIV element with id="output".
 const div = document.getElementById("output") as HTMLDivElement;
@@ -8,9 +12,12 @@ const renderer = new Renderer(div, Renderer.Backends.SVG);
 renderer.resize(window.innerWidth, 200);
 const context = renderer.getContext();
 
-const generateStave = (oldStave?: Stave): Stave => {
+const svgChildren = (div.firstElementChild as SVGElement).children as HTMLCollectionOf<SVGGElement>;
+
+const generateStave = (oldStave?: Flow.Stave): Flow.Stave => {
     const width = oldStave ? oldStave.getWidth() : 0;
-    const x = oldStave ? oldStave.getX() : window.innerWidth / 2;
+    // const x = oldStave ? oldStave.getX() : window.innerWidth / 2;
+    const x = oldStave ? oldStave.getX() : 0;
     const staveMeasure = new Stave(width + x, 0, 200);
     if (!oldStave) {
         staveMeasure.addClef("treble").setContext(context).draw();
@@ -27,6 +34,19 @@ const generateStave = (oldStave?: Stave): Stave => {
     return staveMeasure
 }
 const staveMeasure1 = generateStave();
-const staveMeasure2 = generateStave(staveMeasure1);
-const staveMeasure3 = generateStave(staveMeasure2);
-const staveMeasure4 = generateStave(staveMeasure3);
+
+let scroll = 0;
+let distanceInPx = 100;
+let durationInMs = 1000;
+function animateWidth(now: number) {
+    requestAnimationFrame(animateWidth);
+    scroll = now * distanceInPx / durationInMs;
+
+    Array.from(svgChildren).forEach((c) => {
+        c.setAttribute('transform', `translate(${scroll} 0)`);
+    });
+
+}
+requestAnimationFrame(animateWidth);
+
+
